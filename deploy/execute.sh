1e13d93a6e6e01fc696e39536728348baeb51c0f
Huynh Minh Tan <minhtan.itdev@gmail.com>
2020-02-21 20:34:16 +0700

    update file execute

--- deploy/execute.sh
+++ deploy/execute.sh
@@ -1,2 +1,18 @@
+export PATH=/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
+pwd
+
+WP=/cbserver/foodsys/foodsys-client
+
+rm -rf $WP/*
+cp -R ./* $WP/
+cd $WP
+pwd
+
 yarn install
 yarn start
+
+#rm -rf /cbserver/foodsys/foodsys-client/*
+#cp -R ./dist/* /cbserver/foodsys/foodsys-client/
+
+
+
