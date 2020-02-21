b14ec949d39ea92c2473237394a53afba6c7246b
Huynh Minh Tan <minhtan.itdev@gmail.com>
2020-02-21 19:24:48 +0700

    add folder build

---
+++ dist/precache-manifest.571c04705a99d5d0631651b4f58bd4de.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = [
+  {
+    "revision": "d004aeea82a74253453b",
+    "url": "https://cdn.antd-admin.zuiidea.com/async.c2585d86.chunk.css"
+  },
+  {
+    "revision": "d004aeea82a74253453b",
+    "url": "https://cdn.antd-admin.zuiidea.com/async.7bf6c420.async.js"
+  },
+  {
+    "revision": "d76b1935fb7f43475fa1",
+    "url": "https://cdn.antd-admin.zuiidea.com/antd.b7ad6281.chunk.css"
+  },
+  {
+    "revision": "d76b1935fb7f43475fa1",
+    "url": "https://cdn.antd-admin.zuiidea.com/antd.6e220467.async.js"
+  },
+  {
+    "revision": "257199f2701a8f4193a5",
+    "url": "https://cdn.antd-admin.zuiidea.com/i18n-0.63489de0.async.js"
+  },
+  {
+    "revision": "d233508e8b66539b36ac",
+    "url": "https://cdn.antd-admin.zuiidea.com/i18n-1.65a0626f.async.js"
+  },
+  {
+    "revision": "547dcd3c9bec500f887e",
+    "url": "https://cdn.antd-admin.zuiidea.com/i18n-2.8fe3da7e.async.js"
+  },
+  {
+    "revision": "948466889cb0ab8610b5",
+    "url": "https://cdn.antd-admin.zuiidea.com/layouts__index.b2e1a729.chunk.css"
+  },
+  {
+    "revision": "948466889cb0ab8610b5",
+    "url": "https://cdn.antd-admin.zuiidea.com/layouts__index.9e640584.async.js"
+  },
+  {
+    "revision": "5e03b3a41287b032996c",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__UIElement__editor__index.28bf2632.async.js"
+  },
+  {
+    "revision": "2f00aab1309ca3f3fb6a",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__ECharts__index.67b0dae9.chunk.css"
+  },
+  {
+    "revision": "2f00aab1309ca3f3fb6a",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__ECharts__index.e39e489d.async.js"
+  },
+  {
+    "revision": "95199972a552d2d18623",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__Recharts__index.d32cb7c7.chunk.css"
+  },
+  {
+    "revision": "95199972a552d2d18623",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__Recharts__index.33c2b08e.async.js"
+  },
+  {
+    "revision": "8fc434c336397f91a328",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__highCharts__index.64d450b3.chunk.css"
+  },
+  {
+    "revision": "8fc434c336397f91a328",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__chart__highCharts__index.fa3d6336.async.js"
+  },
+  {
+    "revision": "0a8ab463bff771e17feb",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__dashboard__index.34069513.chunk.css"
+  },
+  {
+    "revision": "0a8ab463bff771e17feb",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__dashboard__index.984c53ea.async.js"
+  },
+  {
+    "revision": "ec49d456f3899cc7b0c1",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__dashboard__model.js.8828c9ca.async.js"
+  },
+  {
+    "revision": "a877d77e61667cd19c45",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__feedback__index.3ac6ef42.chunk.css"
+  },
+  {
+    "revision": "a877d77e61667cd19c45",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__feedback__index.5df703c0.async.js"
+  },
+  {
+    "revision": "6fec59f96ba998f62c01",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__feedback__model.js.e9153443.async.js"
+  },
+  {
+    "revision": "d426fb93d380c2346ed1",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__index.2f4dfcd2.async.js"
+  },
+  {
+    "revision": "deaf2eb3ee774ce11f0f",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__login__index.3836009b.chunk.css"
+  },
+  {
+    "revision": "deaf2eb3ee774ce11f0f",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__login__index.309589ad.async.js"
+  },
+  {
+    "revision": "6106f21d18216889cf9b",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__login__model.js.396b1ec7.async.js"
+  },
+  {
+    "revision": "a2d051a7018ea7deb57d",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__menu__index.3b007ffa.chunk.css"
+  },
+  {
+    "revision": "a2d051a7018ea7deb57d",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__menu__index.0f55bb19.async.js"
+  },
+  {
+    "revision": "397ff136123a5a1d7231",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order__$id__model.js.39b6d951.async.js"
+  },
+  {
+    "revision": "4298d29953818cbf2772",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order___id__index.d734801e.chunk.css"
+  },
+  {
+    "revision": "4298d29953818cbf2772",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order___id__index.1b2a69c5.async.js"
+  },
+  {
+    "revision": "209096871cbc76a248b9",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order__index.a399e7af.chunk.css"
+  },
+  {
+    "revision": "209096871cbc76a248b9",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order__index.64ebd3da.async.js"
+  },
+  {
+    "revision": "03d695efd39708f843ab",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__order__model.js.1a36d5d5.async.js"
+  },
+  {
+    "revision": "51d504b5227ff4d065e8",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__post____index.a1a0c53f.chunk.css"
+  },
+  {
+    "revision": "51d504b5227ff4d065e8",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__post____index.dd502453.async.js"
+  },
+  {
+    "revision": "e2b61f4de64aecf16d09",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__post____model.js.a531d7f2.async.js"
+  },
+  {
+    "revision": "cc0240d141e4c6b9a64b",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__promotion__index.79a88cf3.chunk.css"
+  },
+  {
+    "revision": "cc0240d141e4c6b9a64b",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__promotion__index.1be5bf62.async.js"
+  },
+  {
+    "revision": "6db0468b3451dd594955",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__request__index.b8295ae3.chunk.css"
+  },
+  {
+    "revision": "6db0468b3451dd594955",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__request__index.a303e4fc.async.js"
+  },
+  {
+    "revision": "2b67b471ab1667763483",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__reservation__index.af402873.chunk.css"
+  },
+  {
+    "revision": "2b67b471ab1667763483",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__reservation__index.923a31eb.async.js"
+  },
+  {
+    "revision": "f20ea1a68a716f19aa29",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__reservation__model.js.8fde5085.async.js"
+  },
+  {
+    "revision": "5cb1ee6d7072d2104652",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__user__$id__models__detail.js.7046bef7.async.js"
+  },
+  {
+    "revision": "d770e68bcafe740c4850",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__user___id__index.82333a41.chunk.css"
+  },
+  {
+    "revision": "d770e68bcafe740c4850",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__user___id__index.9570670f.async.js"
+  },
+  {
+    "revision": "94291d70e8badcae1c02",
+    "url": "https://cdn.antd-admin.zuiidea.com/p__user__model.js.38e10ce2.async.js"
+  },
+  {
+    "revision": "c534b8b52bc4651ad767",
+    "url": "https://cdn.antd-admin.zuiidea.com/umi.fa9b5d56.css"
+  },
+  {
+    "revision": "c534b8b52bc4651ad767",
+    "url": "https://cdn.antd-admin.zuiidea.com/umi.722e3f10.js"
+  },
+  {
+    "revision": "543334fb0f9aa0a894a5eb63d7c7125e",
+    "url": "https://cdn.antd-admin.zuiidea.com/static/user-background.543334fb.png"
+  },
+  {
+    "revision": "fe1aba3b11454517047d78d4adb7ebb3",
+    "url": "https://cdn.antd-admin.zuiidea.com/index.html"
+  },
+  {
+    "revision": "4a52512e9fa2b4741f371fee88abc9d9",
+    "url": "https://cdn.antd-admin.zuiidea.com/america.svg"
+  },
+  {
+    "revision": "949490b4c6a8d424986311db79ca6cc4",
+    "url": "https://cdn.antd-admin.zuiidea.com/china.svg"
+  },
+  {
+    "revision": "6380fd2b05446333bc5a5a2d43d4a476",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@128.png"
+  },
+  {
+    "revision": "4de769202c3de12eb1e01c990969e786",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@144.png"
+  },
+  {
+    "revision": "ca512a42280545acb724b3c11e0c679b",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo.svg"
+  },
+  {
+    "revision": "1afcb8aa128dc7161de74ca9c5d7a544",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@152.png"
+  },
+  {
+    "revision": "f088a2fc4cd668246b76ee53bb5c4ccf",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@192.png"
+  },
+  {
+    "revision": "c0e293d83f7b3f4258951e458c6a6a5e",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@72.png"
+  },
+  {
+    "revision": "ab2b389f9b903872cacf46e33bbed76c",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@96.png"
+  },
+  {
+    "revision": "5bc1f8e042b2424fd6b6950190381087",
+    "url": "https://cdn.antd-admin.zuiidea.com/portugal.svg"
+  },
+  {
+    "revision": "aac84280dbf5be13fa4a4af9baea1b30",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@384.png"
+  },
+  {
+    "revision": "fa34d4b24a5e581755f947b398ac92a6",
+    "url": "https://cdn.antd-admin.zuiidea.com/logo/logo@512.png"
+  },
+  {
+    "revision": "5b29f8ca48ae4b977fa89e912762651d",
+    "url": "https://cdn.antd-admin.zuiidea.com/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "https://cdn.antd-admin.zuiidea.com/pwacompat.min.js"
+  }
+];
